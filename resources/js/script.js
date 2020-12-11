6d0278d3f02a05c91e780fa4c8fd928f8d48694f
Aditya Saxena <asaxena1205@gmail.com>
2020-12-11 11:09:37 +0530

    made the navigation responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -90,4 +90,21 @@ $(document).ready(function() {
     });
 
 
+    /* Mobile Navigatin Icon */
+    $('.js--nav-icon').click(function() {
+        var nav= $('.js--main-nav');
+        var icon= $('.js--nav-icon ion-icon');
+
+        nav.slideToggle(200);
+        
+        if ( icon.attr('name') == 'menu-outline' ) {
+            icon.attr("name", "close-outline");
+        } else {
+            icon.attr("name", "menu-outline");
+        }
+
+    })
+
+
+
 });
